280e785c58e3f7d7fe31803f2ad2f9d1bcbc183d
davidwolski <dawolski@gmail.com>
2016-08-22 23:03:05 -0400

    Change custom cdf to version 20.

--- custom_install.R
+++ custom_install.R
@@ -1,13 +1,13 @@
-custom_install_packages <- c("http://mbni.org/customcdf/19.0.0/entrezg.download/hgu133a2hsentrezgcdf_19.0.0.tar.gz",   # hgu133a2 cdf
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/hgu133a2hsentrezgprobe_19.0.0.tar.gz",  # hgu133a2 probe sequences
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/pd.hgu133a2.hs.entrezg_19.0.0.tar.gz",  # hgu133a2 probe map
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/hgu133a2hsentrezg.db_19.0.0.tar.gz", 	 # hgu133a2 database
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/hthgu133ahsentrezgcdf_19.0.0.tar.gz",   # hthgu133a cdf
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/hthgu133ahsentrezgprobe_19.0.0.tar.gz",  # hthgu133a probe sequences
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/pd.hthgu133a.hs.entrezg_19.0.0.tar.gz",  # hthgu133a probe map
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/hthgu133ahsentrezg.db_19.0.0.tar.gz", 	 # hthgu133a database
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/u133aaofav2hsentrezgcdf_19.0.0.tar.gz",   # u133aaofav2 cdf
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/u133aaofav2hsentrezgprobe_19.0.0.tar.gz",  # u133aaofav2 probe sequences
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/pd.u133aaofav2.hs.entrezg_19.0.0.tar.gz",  # u133aaofav2 probe map
-                            "http://mbni.org/customcdf/19.0.0/entrezg.download/u133aaofav2hsentrezg.db_19.0.0.tar.gz" 	 # u133aaofav2 database
+custom_install_packages <- c("http://mbni.org/customcdf/20.0.0/entrezg.download/hgu133a2hsentrezgcdf_20.0.0.tar.gz",   # hgu133a2 cdf
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/hgu133a2hsentrezgprobe_20.0.0.tar.gz",  # hgu133a2 probe sequences
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/pd.hgu133a2.hs.entrezg_20.0.0.tar.gz",  # hgu133a2 probe map
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/hgu133a2hsentrezg.db_20.0.0.tar.gz", 	 # hgu133a2 database
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/hthgu133ahsentrezgcdf_20.0.0.tar.gz",   # hthgu133a cdf
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/hthgu133ahsentrezgprobe_20.0.0.tar.gz",  # hthgu133a probe sequences
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/pd.hthgu133a.hs.entrezg_20.0.0.tar.gz",  # hthgu133a probe map
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/hthgu133ahsentrezg.db_20.0.0.tar.gz", 	 # hthgu133a database
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/u133aaofav2hsentrezgcdf_20.0.0.tar.gz",   # u133aaofav2 cdf
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/u133aaofav2hsentrezgprobe_20.0.0.tar.gz",  # u133aaofav2 probe sequences
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/pd.u133aaofav2.hs.entrezg_20.0.0.tar.gz",  # u133aaofav2 probe map
+                            "http://mbni.org/customcdf/20.0.0/entrezg.download/u133aaofav2hsentrezg.db_20.0.0.tar.gz" 	 # u133aaofav2 database
 )
